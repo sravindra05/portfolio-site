59657d4fcae2f6f914b46708bb578a15c31e1e0e
Sarang Ravindra <sarang7822ravindra@gmail.com>
2019-11-25 11:40:18 +0530

    New file incorporation

--- painting.js
+++ painting.js
@@ -1,19 +1,13 @@
-var file_list=[
-    "01_RT16.jpg",
-    "14_RT16.jpg",
-    "A Boat.jpg",
-    "Face.jpg",
-
-]
+var file_number=15
 var c1=document.getElementById('c1');
 var c2=document.getElementById('c2');
 var c3=document.getElementById('c3');
 var c4=document.getElementById('c4');
 var col_array=[c1,c2,c3,c4];
-var path="assets/paintings/"
-for(i=0;i<file_list.length;i++){
+var path="assets/paintings/painting"
+for(i=1;i<=file_number;i++){
     var im=document.createElement("img");
     im.setAttribute("class","materialboxed");
-    im.setAttribute("src",path+file_list[i]);
-    col_array[i%4].appendChild(im)
+    im.setAttribute("src",path+i+'.jpg');
+    col_array[(i-1)%4].appendChild(im)
 }
--- portrait.js
+++ portrait.js
@@ -1,21 +1,14 @@
-var file_list=[
-    "APPA.jpg",
-    "BANDIPUROLDMAN.jpg",
-    "DeepakRL.jpg",
-    "RafiqKhan.JPG",
-    "SusheelaKulkarni.jpg",
-
-]
+var file_number=13
 var c1=document.getElementById('c1');
 var c2=document.getElementById('c2');
 var c3=document.getElementById('c3');
 var c4=document.getElementById('c4');
 var col_array=[c1,c2,c3,c4];
-var path="assets/portraits/"
-for(i=0;i<file_list.length;i++){
+var path="assets/portraits/portrait"
+for(i=1;i<=file_number;i++){
     var im=document.createElement("img");
     im.setAttribute("class","materialboxed");
-    im.setAttribute("src",path+file_list[i]);
-    col_array[i%4].appendChild(im)
+    im.setAttribute("src",path+i+'.jpg');
+    col_array[(i-1)%4].appendChild(im)
 }
 
